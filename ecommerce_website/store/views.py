d4fc91bb3e3416b73ddea910d5cf9a93ddfec5f0
divyansh-tamrakar <divyansh.tamrakar17@gmail.com>
2022-01-13 18:45:44 +0530

    checkout updated

--- ecommerce_website/store/views.py
+++ ecommerce_website/store/views.py
@@ -22,7 +22,7 @@ def store(request):
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
-    context = {'products': products, 'number': cartItems}
+    context = {'products': products, 'order': order, 'number': cartItems}
     return render(request, 'store/store.html', context)
 
 
@@ -35,7 +35,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_items': 0, 'get_cart_total': 0}
+        order = {'get_cart_items': 0, 'get_cart_total': 0, 'shipping': False}
         cartItems = order['get_cart_items']
     total = 0
     number = 0
@@ -43,7 +43,7 @@ def cart(request):
         total += item.product.price * item.quantity
         number += item.quantity
     
-    context = {'items': items, 'total': total, 'number': cartItems}
+    context = {'items': items, 'total': total, 'order': order, 'number': cartItems}
     return render(request, 'store/cart.html', context)
 
 
@@ -57,13 +57,13 @@ def checkout(request):
         items = []
         order = {'get_cart_items': 0, 'get_cart_total': 0}
         cartItems = order['get_cart_items']
+    
     total = 0
-    number = 0
     for item in items:
         total += item.product.price * item.quantity
-        number += item.quantity
+        
 
-    context = {'items': items, 'total': total, 'number': cartItems}
+    context = {'items': items, 'total': total, 'order': order, 'number': cartItems, 'shipping': False}
 
     return render(request, 'store/checkout.html', context)
 
